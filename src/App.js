9887b684623aa3fdb8e44e10c3ded2c26bfff5db
Stas9364 <stas-lisovskii@mail.ru>
2022-05-28 03:14:32 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./Header/Header";
+import {Main} from "./Main/Main";
+import {Skills} from "./Skills/Skills";
+import {Projects} from "./My projects/Projects";
+import {Contacts} from "./Contacts/Contacts";
+import {Footer} from "./Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Skills/>
+            <Projects/>
+            <Contacts/>
+            <Footer/>
+        </div>
+    )
+};
