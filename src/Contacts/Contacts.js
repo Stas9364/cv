5326e52a9db4bf49d03cb9722477ff40937b4cdb
Stas9364 <stas-lisovskii@mail.ru>
2022-08-21 18:06:06 +0300

    add feedback form

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Contacts.module.scss';
 import {Title} from '../Commons/Title/Title';
 import phone from '../assets/img/phone-call.png';
-import email from '../assets/img/email.png';
+import emailIMG from '../assets/img/email.png';
 import location from '../assets/img/location.png';
+import thanks from './../assets/img/thanks.jpg';
 import {ContactsData} from "./ContactsData/ContactsData";
+import axios from 'axios';
 
 export const Contacts = () => {
+    const [name, setName] = useState('');
+    const [contacts, setContacts] = useState('');
+    const [message, setMessage] = useState('');
+    const [isSent, setIsSent] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setIsSent(true);
+
+        axios.post('https://portfolio-node-js-server.herokuapp.com/send-message',
+            {name, contacts, message})
+            .then(() => {
+                setTimeout(() => {
+                    setIsSent(false);
+                }, 2000)
+            });
+
+        console.log('name', name)
+        console.log('contacts', contacts)
+        console.log('message', message)
+
+        setName('');
+        setMessage('');
+        setContacts('');
+    };
+
     return (
         <div className={styles.contactsBlock}>
             <div className={`${styles.container} ${styles.contactsWrapper}`}>
@@ -25,7 +54,7 @@ export const Contacts = () => {
                             contact={'+375333896666'}
                         />
                         <ContactsData
-                            img={email}
+                            img={emailIMG}
                             description={'Email'}
                             contact={'stas-lisovskiy@mail.ru'}
                         />
@@ -36,15 +65,33 @@ export const Contacts = () => {
                         />
                     </div>
 
-                    <form action="#" className={styles.formWrapper}>
-                        <div>
-                            <input type="text" placeholder='Your Name'/>
-                            <input type="text" placeholder='Your Email'/>
+                    {isSent
+                        ? <div className={styles.gratitude}>
+                            <img src={thanks} alt="gratitude"/>
                         </div>
-                        <textarea placeholder='Your Message'/>
-                        <button type="submit">Send message</button>
-                    </form>
 
+                        : <form  className={styles.formWrapper} onSubmit={handleSubmit}>
+                        <div>
+                            <input
+                                type="text"
+                                placeholder='Your Name'
+                                value={name}
+                                onChange={e => setName(e.currentTarget.value)}
+                            />
+                            <input
+                                type="text"
+                                placeholder='Your Contacts'
+                                value={contacts}
+                                onChange={e => setContacts(e.currentTarget.value)}
+                            />
+                        </div>
+                        <textarea
+                            placeholder='Your Message'
+                            value={message}
+                            onChange={e => setMessage(e.currentTarget.value)}
+                        />
+                        <button type="submit">Send message</button>
+                    </form>}
                 </div>
             </div>
         </div>
